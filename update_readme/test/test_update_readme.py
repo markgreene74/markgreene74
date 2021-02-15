a118b59e6ea955c27edc70ac46c886cd9225f853
markgreene74 <g.cunsolo3@gmail.com>
2021-02-15 00:11:13 +0000

    small changes and formatting

--- update_readme/test/test_update_readme.py
+++ update_readme/test/test_update_readme.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 import update_readme as updt
 
+
 def test_add_last_updated():
     _mock_date = "2021-01-01T00:00:00"
     with freeze_time(_mock_date):
-        print(datetime.now())
         assert datetime.isoformat(updt.add_last_updated()) == _mock_date
--- update_readme/update_readme.py
+++ update_readme/update_readme.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def add_last_updated():
     _today = datetime.now()
     return _today
