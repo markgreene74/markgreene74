c9d0559eeb251f58cea731543cc0d14cf979b751
markgreene74 <g.cunsolo3@gmail.com>
2021-03-07 17:34:58 +0000

    template README from jinja and name changes for consistency

--- update_readme/test/test_update_readme.py
+++ update_readme/test/test_update_readme.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+from shutil import copytree
 
 from freezegun import freeze_time
 from datetime import datetime
@@ -7,16 +8,19 @@ from datetime import datetime
 import update_readme as updt
 
 FILENAME = "README.md"
+TEMPLATE = "templates/README.md.jinja"
+TEMPLATE_DIR = "templates"
 DATETIME_FMT = "%Y-%m-%d %H:%M"
 
 
-def test_last_updated():
+def test_get_datetime():
     _mock_date = "2021-01-01 00:00"
     with freeze_time(_mock_date):
-        assert updt.last_updated() == _mock_date
+        assert updt.get_datetime() == _mock_date
 
 
 def test_readme(tmp_path):
+    copytree(TEMPLATE_DIR, os.path.join(tmp_path, TEMPLATE_DIR))
     os.chdir(tmp_path)
 
     updt.write_readme()
--- update_readme/update_readme.py
+++ update_readme/update_readme.py
@@ -1,17 +1,27 @@
 from datetime import datetime
+from jinja2 import Template
 
 FILENAME = "README.md"
+TEMPLATE = "templates/README.md.jinja"
+DATETIME_FMT = "%Y-%m-%d %H:%M"
 
 
-def last_updated():
-    _today = datetime.now().strftime("%Y-%m-%d %H:%M")
+def get_datetime():
+    """Get datetime.now() in a specific format"""
+    _today = datetime.now().strftime(DATETIME_FMT)
     return _today
 
 
 def write_readme():
-    _today = last_updated()
+    """Wtite the readme file from a jinja template"""
+    with open(TEMPLATE) as f:
+        readme_jinja = f.read()
+
+    template = Template(readme_jinja)
+    readme_rendered = template.render(last_updated=get_datetime())
+
     with open(FILENAME, "w") as f:
-        f.write(f"Last updated: {_today}")
+        f.write(readme_rendered)
 
 
 if __name__ == "__main__":
