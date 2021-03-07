0a59d4978e69afec2c8eb142fd721bee5609f244
markgreene74 <g.cunsolo3@gmail.com>
2021-03-07 16:47:23 +0000

    add last updated date time to README

--- update_readme/test/test_update_readme.py
+++ update_readme/test/test_update_readme.py
@@ -1,12 +1,28 @@
 import pytest
+import os
 
 from freezegun import freeze_time
 from datetime import datetime
 
 import update_readme as updt
 
+FILENAME = "README.md"
+DATETIME_FMT = "%Y-%m-%d %H:%M"
 
-def test_add_last_updated():
-    _mock_date = "2021-01-01T00:00:00"
+
+def test_last_updated():
+    _mock_date = "2021-01-01 00:00"
     with freeze_time(_mock_date):
-        assert datetime.isoformat(updt.add_last_updated()) == _mock_date
+        assert updt.last_updated() == _mock_date
+
+
+def test_readme(tmp_path):
+    os.chdir(tmp_path)
+
+    updt.write_readme()
+    _today = datetime.now().strftime(DATETIME_FMT)
+
+    with open(FILENAME) as temp_file:
+        readme_file = temp_file.read()
+
+    assert f"Last updated: {_today}" in readme_file
--- update_readme/update_readme.py
+++ update_readme/update_readme.py
@@ -1,6 +1,18 @@
 from datetime import datetime
 
+FILENAME = "README.md"
 
-def add_last_updated():
-    _today = datetime.now()
+
+def last_updated():
+    _today = datetime.now().strftime("%Y-%m-%d %H:%M")
     return _today
+
+
+def write_readme():
+    _today = last_updated()
+    with open(FILENAME, "w") as f:
+        f.write(f"Last updated: {_today}")
+
+
+if __name__ == "__main__":
+    write_readme()
