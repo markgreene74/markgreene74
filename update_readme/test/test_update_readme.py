0b5999e78c90e44f63ffdaa51bbc4d8c9249824d
markgreene74 <markgreene74@users.noreply.github.com>
2021-12-04 17:25:09 +0000

    Improve conferences section (#4)
    * csv for all the conferences
    
    * refactor csv, add new conferences, add skeleton to process csv
    
    * add tests, add first version of build conference function
    
    * enable workflows to push changes
    
    * code style: black
    
    * refactor build_conf_list()
    
    * template now use data from a csv file, update test
    
    Co-authored-by: run-black-bot <actions@users.noreply.github.com>

--- update_readme/test/test_update_readme.py
+++ update_readme/test/test_update_readme.py
@@ -1,17 +1,20 @@
 import pytest
-import os
-from shutil import copytree
 
+import os
+from shutil import copytree, copyfile
+from csv import DictReader
+from datetime import datetime as dt
 from freezegun import freeze_time
-from datetime import datetime
 
 import update_readme as updt
 
 FILENAME = "README.md"
 README_PATH = os.path.join("../", FILENAME)
 TEMPLATE = "templates/README.md.jinja"
-TEMPLATE_DIR = "templates"
+FILENAME_CSV = "test/test_data.csv"
 DATETIME_FMT = "%Y-%m-%d %H:%M"
+TEMPLATE_DIR = "templates"
+DATA_DIR = "data"
 
 
 def test_get_datetime():
@@ -20,16 +23,34 @@ def test_get_datetime():
         assert updt.get_datetime() == _mock_date
 
 
+def test_conference_year():
+    _year = "2021"
+    _new, _old = updt.build_conf_list(FILENAME_CSV, _year)
+    assert len(_new) == 2
+    assert len(_old) == 3
+
+
+def test_build_conf_list():
+    _new, _old = updt.build_conf_list(FILENAME_CSV)
+    assert len(_new) == 5
+    assert len(_old) == 0
+
+
 def test_readme(tmp_path):
     base_dir = os.path.join(tmp_path, "update_readme")
     os.mkdir(base_dir)
+    # copy the template and data dir in the tmp path
     copytree(TEMPLATE_DIR, os.path.join(base_dir, TEMPLATE_DIR))
+    copytree(DATA_DIR, os.path.join(base_dir, DATA_DIR))
+    # replace the conferences file with the test file
+    copyfile(FILENAME_CSV, os.path.join(base_dir, DATA_DIR, "conferences.csv"))
     os.chdir(base_dir)
 
     updt.write_readme()
-    _today = datetime.now().strftime(DATETIME_FMT)
+    _today = dt.now().strftime(DATETIME_FMT)
 
     with open(README_PATH) as temp_file:
         readme_file = temp_file.read()
 
     assert f"Last updated: {_today}" in readme_file
+    assert "Conference 5" in readme_file
--- update_readme/update_readme.py
+++ update_readme/update_readme.py
@@ -1,26 +1,74 @@
-from datetime import datetime
-from jinja2 import Template
 import os
+from csv import DictReader
+from datetime import datetime as dt
+from jinja2 import Template
 
 FILENAME = "README.md"
 README_PATH = os.path.join("../", FILENAME)
 TEMPLATE = "templates/README.md.jinja"
+FILENAME_CSV = "data/conferences.csv"
 DATETIME_FMT = "%Y-%m-%d %H:%M"
 
 
 def get_datetime():
     """Get datetime.now() in a specific format"""
-    _today = datetime.now().strftime(DATETIME_FMT)
+    _today = dt.now().strftime(DATETIME_FMT)
     return _today
 
 
+def build_conf_list(csv_file=FILENAME_CSV, year=None):
+    """Parse the csv file, extract and process the data"""
+    with open(csv_file) as f:
+        data = DictReader(f)
+        all_conferences = [row for row in data]
+    # "conference","start","end","online","fee","registered","attended","website"
+
+    # process the data
+    for conf in all_conferences:
+        # replace "True"/"False" with a boolean
+        for k in ["online", "fee", "registered", "attended"]:
+            conf[k] = True if conf[k].lower() == "true" else False
+        # replace str with a proper datetime.date object,
+        # if it's a one day conference set start date to None
+        conf["start"] = (
+            None
+            if conf["start"] == conf["end"]
+            else dt.strptime(conf["start"], "%d/%m/%Y").date()
+        )
+        conf["end"] = dt.strptime(conf["end"], "%d/%m/%Y").date()
+        # add a key/value with the n of days, set to 1 for one-day conferences
+        conf["days"] = (conf["end"] - conf["start"]).days + 1 if conf["start"] else 1
+
+    # now we can order the conferences, for example by end date
+    all_conferences.sort(key=lambda x: x["end"], reverse=False)
+
+    # decide what to return based on if the arg `year` was provided
+    if year:
+        y = int(year)
+        conf_new = [conf for conf in all_conferences if conf["end"].year >= y]
+        conf_old = [conf for conf in all_conferences if conf["end"].year < y]
+    else:
+        conf_new = all_conferences
+        conf_old = []
+
+    return conf_new, conf_old
+
+
 def write_readme():
-    """Wtite the readme file from a jinja template"""
+    """Write the readme file from a jinja template"""
     with open(TEMPLATE) as f:
         readme_jinja = f.read()
 
+    current_year = dt.now().year
+    conf_new, conf_old = build_conf_list(year=current_year)
+
     template = Template(readme_jinja)
-    readme_rendered = template.render(last_updated=get_datetime())
+    readme_rendered = template.render(
+        last_updated=get_datetime(),
+        current_year=current_year,
+        conf_new=conf_new,
+        conf_old=conf_old,
+    )
 
     with open(README_PATH, "w") as f:
         f.write(readme_rendered)
