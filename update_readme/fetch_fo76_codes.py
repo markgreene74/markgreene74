1b2711f472a67be48809e1535e01df658d604607
markgreene74 <markgreene74@users.noreply.github.com>
2023-04-11 00:27:33 +0100

    Update fetch_fo76_codes.py

--- update_readme/fetch_fo76_codes.py
+++ update_readme/fetch_fo76_codes.py
@@ -4,19 +4,19 @@ import re
 
 FILENAME = "fo76"
 FULL_PATH = os.path.join("./", FILENAME)
-NC_URL = "https://nukacrypt.com/"
+URL = "https://www.falloutbuilds.com"
 
 ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-nc_content = str(requests.get(NC_URL, headers={"User-Agent": ua}).content)
-silos = re.findall(r"\>(ALPHA|BRAVO|CHARLIE)\<", nc_content)
-codes = re.findall(r"\>(\d+)\<", nc_content)
+content = str(requests.get(URL, headers={"User-Agent": ua}).content)
+silos = re.findall(r"\>(ALPHA|BRAVO|CHARLIE)\<", content)
+codes = re.findall(r"\>(\d+\s*\d+\s*\d+)\<", content)
 
 # >>> silos
 # ['ALPHA', 'BRAVO', 'CHARLIE']
 # >>> codes
-# ['61436701', '36758567', '79473176']
+# ['841 38 947', '676 23 748', '512 39 897']
 # >>> list(zip(silos, codes))
-# [('ALPHA', '61436701'), ('BRAVO', '36758567'), ('CHARLIE', '79473176')]
+# [('ALPHA', '841 38 947'), ('BRAVO', '676 23 748'), ('CHARLIE', '512 39 897')]
 
 s_c_list = [f"{s}:{c}" for s, c in list(zip(silos, codes))]
 file_content = "\n".join(s_c_list)
